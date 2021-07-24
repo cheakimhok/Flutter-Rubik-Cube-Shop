2add012985395e03b33ce2c1e7e2c63326752281
Cheakimhok Mao <mao.cheakimhok@gmail.com>
2021-07-24 21:29:16 +0700

    cart screen api

--- lib/screens/cart/cart.dart
+++ lib/screens/cart/cart.dart
@@ -1,6 +1,10 @@
+import 'package:rubik_cube_shop/models/Cart.dart';
 import 'package:rubik_cube_shop/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:rubik_cube_shop/bottom_bar.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:rubik_cube_shop/size.dart';
 
 class CartScreen extends StatefulWidget {
   @override
@@ -8,8 +12,70 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+
+  List<Cart> fetchCart = [];
+  bool _isLoading = true;
+  bool _isError = false;
+
+  Future<Cart> fetchCartData() async {
+    final url = Uri.parse('http://localhost:3000/cart');
+    final response = await get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+
+      setState(() {
+        for (Map i in data) {
+          fetchCart.add(Cart.fromJson(i));
+        }
+        _isLoading = false;
+      });
+    } else {
+      _isError = true;
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCartData();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    if (_isLoading) {
+      return Center(child: CircularProgressIndicator());
+    }
+    
+    if (_isError) {
+      return Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(30)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.error,
+              size: getProportionateScreenWidth(45),
+              color: Colors.grey,
+            ),
+            SizedBox(height: getProportionateScreenWidth(10)),
+            Text(
+              'Error while loading data from the server. Please try again later.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: getProportionateScreenWidth(14)
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -71,122 +137,126 @@ class _CartScreenState extends State<CartScreen> {
       bottomNavigationBar: BottomBar(),
     );
   }
-}
 
-Widget cartBody () {
-  return ListView(
-    children: <Widget> [
-      Column(children: List.generate(3, (index) {
-        return Padding(
-          padding: EdgeInsets.only(right: 25, left: 25, bottom: 10, top: 15),
+  Widget cartBody () {
+    return ListView(
+      children: <Widget> [
+        Column(
+          children: List.generate(fetchCart.length, (index) {
+          return Padding(
+            padding: EdgeInsets.only(right: 25, left: 25, bottom: 10, top: 15),
+            child: Row(
+              children: <Widget> [
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: BoxDecoration(
+                    // color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage(
+                        '${fetchCart[index].productImage}', 
+                      ), 
+                      fit: BoxFit.contain,
+                    ) 
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget> [
+                      Row(
+                        children: <Widget> [
+                          SizedBox(width: 20),
+                          Text(
+                            '${fetchCart[index].productName}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(width: 90),
+                          Icon(
+                            Icons.close,
+                            size: 28,
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: <Widget> [
+                          SizedBox(height: 50),
+                          SizedBox(width: 20),
+                          Text(
+                            '${fetchCart[index].productPrice}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(width: 30),
+                          Text(
+                            'x${fetchCart[index].productAmount}',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w500
+                            )
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        }),),
+        SizedBox(height: 60), 
+        Padding(
+          padding: EdgeInsets.only(left:25, right: 25),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween, 
             children: <Widget> [
-              Container(
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  // color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/3x3.jpeg"),
-                    fit: BoxFit.contain,
-                  ) 
+              Text(
+                "Total",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500
                 ),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget> [
-                    Row(
-                      children: <Widget> [
-                        SizedBox(width: 20),
-                        Text(
-                          "Gan 356 RS",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        SizedBox(width: 90),
-                        Icon(
-                          Icons.close,
-                          size: 28,
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: <Widget> [
-                        SizedBox(height: 50),
-                        SizedBox(width: 20),
-                        Text(
-                          "\$16.99",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        SizedBox(width: 30),
-                        Text(
-                          "x1",
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w500
-                          )
-                        )
-                      ],
-                    )
-                  ],
+              Text(
+                "\$16.99",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600
                 ),
               )
             ],
           ),
-        );
-      }),),
-      SizedBox(height: 60), 
-      Padding(
-        padding: EdgeInsets.only(left:25, right: 25),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween, 
-          children: <Widget> [
-            Text(
-              "Total",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w500
-              ),
-            ),
-            Text(
-              "\$16.99",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w600
-              ),
-            )
-          ],
         ),
-      ),
-      SizedBox(height: 30),
-      Center(
-        child: Container(
-          width: 380,
-          height: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25.0),
-            color: Color.fromRGBO(0, 161, 233, 1)
-          ),
-          child: Center(
-            child: Text('Checkout',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,   
-              ),
+        SizedBox(height: 30),
+        Center(
+          child: Container(
+            width: 380,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25.0),
+              color: Color.fromRGBO(0, 161, 233, 1)
+            ),
+            child: Center(
+              child: Text('Checkout',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,   
+                ),
+              )
             )
           )
         )
-      )
-    ]
-  );
+      ]
+    );
+  }
 }
+
